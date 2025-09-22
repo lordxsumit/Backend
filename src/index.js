d7f9a63192ce5e5b35edc0724b09274d14286dc5
lordxsumit <sum56it@gmail.com>
2025-09-22 21:08:10 +0530

    Utilities code discussion

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
+import express from 'express'
+const app = express()
+
 import dotenv from 'dotenv';
 import connectDB from "./db/db.js";
-
 
 dotenv.config({
     path: './env'
 })
 
 connectDB()
-
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("MONGODB connection failed !!!", err);
+})
 
 
 
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,27 @@
+const asyncHandler = (requestHandler) => {                                      // Using -> Promise
+    (req, res, next) => {
+        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
+    }
+};
+
+
+export {asyncHandler}
+
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (func) => () => {}
+// const asyncHandler = (func) => async () => {}
+
+
+
+// const asyncHandler = (fn) => async (req, res, next) => {                     // Without using -> Promise
+//     try {
+//         await fn(req, res, next)
+//     } catch (error) {
+//         res.status(error.code || 500).json({
+//             success: false,
+//             message: error.message
+//         })
+//     }
+// }
