bb360015c5a26c2c9218c32fa28d59f54b790153
lordxsumit <sum56it@gmail.com>
2026-04-15 11:37:13 +0530

    add get user channel subscribers and subscribed channels functionalities: implement getUserChannelSubscribers and getSubscribedChannels methods with validation and error handling

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -46,6 +46,51 @@ const toggleSubscription = asyncHandler(async (req, res) => {
 })
 
 
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const { channelId } = req.params;
+    if(!channelId){
+        throw new ApiError(400, "Channel ID is required")
+    }
+
+    const subscribers = await subscription.find({ channel: channelId })
+    .populate("subscriber", "username fullName avatar")                     // ".populate() is used so that we can use the functionality of "join" in no-SQL databases like mongoDB.
+    .select("-channel -__v")
+
+    if(!subscribers){
+        throw new ApiError(404, "No subscribers found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscribers, "Subscribers fetched successfully")
+    )
+})
+
+
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params;
+    if(!subscriberId){
+        throw new ApiError(400, "Subscriber ID is required")
+    }
+
+    const subscribedChannels = await subscription.find({ subscriber: subscriberId})
+    .populate("channel", "username fullName avatar")
+    .select("-subscriber -__v")
+
+    if(!subscribedChannels){
+        throw new ApiError(404, "No subscribed channels found")
+    }
+
+    return res
+    .status(200)
+    .json(
+        new ApiResponse(200, subscribedChannels, "Subscribed channels fetched successfully")
+    )
+})
+
 export{
-    toggleSubscription
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
 }
