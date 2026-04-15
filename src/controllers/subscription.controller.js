9fbe40de86b17bbbd9778939a5d503820fb7eb08
lordxsumit <sum56it@gmail.com>
2026-04-15 11:43:21 +0530

    remove unused mongoose import from subscription.controller.js and add playlist.controller.js with initial imports

--- src/controllers/subscription.controller.js
+++ src/controllers/subscription.controller.js
@@ -2,7 +2,6 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { subscription } from "../models/subscription.model.js";
-import mongoose from "mongoose";
 
 
 const toggleSubscription = asyncHandler(async (req, res) => {
